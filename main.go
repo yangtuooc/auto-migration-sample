399245cfad1e8fb12666a6cbaca8cc3757b4bfdc
mawenhao <mawh@bxlsj.com>
2023-04-27 19:55:46 +0800

    feat: build  auto migration sample project

---
+++ main.go
@@ -0,0 +1,9 @@
+package auto_migration_sample
+
+import "auto-migration-sample/store"
+
+func main() {
+
+	store.Init()
+
+}
---
+++ store/store.go
@@ -0,0 +1,11 @@
+package store
+
+import "gorm.io/gorm"
+
+var (
+	db *gorm.DB
+)
+
+func Init() {
+
+}
